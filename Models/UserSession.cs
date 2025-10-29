9e5f7c9efa1ae3b62a5048eef4dcea296211510c
thuanphatce191406-ai <thuanphat.ce191406@gmail.com>
2025-10-29 09:50:40 +0700

    Add files via upload

---
+++ Models/UserSession.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentManagement.Models
+{
+    internal class UserSession : INotifyPropertyChanged
+    {
+        private static UserSession instance = null;
+        private static readonly object instanceLock = new object();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public static UserSession Instance
+        {
+            get
+            {
+                lock (instanceLock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new UserSession();
+                    }
+                    return instance;
+                }
+            }
+        }
+        private UserSession() { }
+        private User _currentUser;
+        public User CurrentUser
+        {
+            get => _currentUser;
+            set
+            {
+                if (_currentUser != value)
+                {
+                    _currentUser = value;
+                    OnPropertyChanged(nameof(CurrentUser));
+                }
+            }
+        }
+        public void Clear()
+        {
+            CurrentUser = null;
+        }
+    }
+}
