0edc037e9be3b9c654e385fd5d64b26ae49f5639
thuanphatce191406-ai <thuanphat.ce191406@gmail.com>
2025-10-29 09:52:10 +0700

    Add files via upload

---
+++ Views/Lecture/Lecture.xaml.cs
@@ -0,0 +1,40 @@
+﻿using StudentManagement.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace StudentManagement.Views.Lecture
+{
+    /// <summary>
+    /// Interaction logic for Lecture.xaml
+    /// </summary>
+    public partial class Lecture : Page
+    {
+        public Lecture()
+        {
+            InitializeComponent();
+            this.DataContext = new LectureViewModel();
+        }
+
+        private void ToHome(object sender, RoutedEventArgs e)
+        {
+            MenuFrame.Content = null;
+        }
+
+        private void ToProfile(object sender, RoutedEventArgs e)
+        {
+            MenuFrame.Content = new ProfilePage();
+        }
+    }
+}
---
+++ Views/Register.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using StudentManagement.ViewModels;
+
+namespace StudentManagement.Views
+{
+    public partial class Register : Window
+    {
+
+        public Register()
+        {
+            InitializeComponent();
+            this.DataContext = new RegisterViewModel();
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left)
+                this.DragMove();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void LoginLabel_Click(object sender, MouseButtonEventArgs e)
+        {
+            Login login = new Login();
+            login.Show();
+            this.Close();
+        }
+    }
+}
